8db2542773a1a63c2f160d0d3b431dce5ac7fb4a
aksemfek <130041317+aksemfek@users.noreply.github.com>
2024-02-01 15:42:46 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	private static Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		double A = scanner.nextInt();
+		double B = scanner.nextInt();
+		
+		double C = A / B;
+		
+		System.out.println(C);
+	}
+}
