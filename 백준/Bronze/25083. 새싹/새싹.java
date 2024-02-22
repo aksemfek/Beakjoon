cce637b45c0321030ec55d0237c35be92d55f44b
aksemfek <130041317+aksemfek@users.noreply.github.com>
2024-02-22 14:43:31 +0900

    [Bronze V] Title: 새싹, Time: 112 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,9 @@
+	
+	public class Main {
+	
+	
+		public static void main(String[] args) {
+			
+			System.out.println("         ,r'\"7"+"\nr`-_   ,'  ,/"+"\n \\. \". L_r'"+"\n   `~\\/"+"\n      |"+"\n      |");		
+		}
+	}
