8ac9ae6b9235b9162c31eacde9a934103dd97d9d
aksemfek <130041317+aksemfek@users.noreply.github.com>
2025-04-09 14:16:55 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 16124 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,15 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args)  {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for(int i=0; i<n; i++){
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            System.out.println("Case #"+ (i+1) +": " + (a+b));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= T; i++) {
+            String[] input = br.readLine().split(" ");
+            int n1 = Integer.parseInt(input[0]);
+            int n2 = Integer.parseInt(input[1]);
+            bw.write("Case #" + i + ": " +(n1+n2) +"\n");
         }
+
+        bw.flush();
+        bw.close();
+
 
     }
 }
