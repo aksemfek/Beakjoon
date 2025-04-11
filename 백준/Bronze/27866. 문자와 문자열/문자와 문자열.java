50c6f66ed45d2970593cbbcf8053afb83e7b9a5d
aksemfek <130041317+aksemfek@users.noreply.github.com>
2025-04-11 14:41:54 +0900

    [Bronze V] Title: 문자와 문자열, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,17 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String str = br.readLine().trim();
-		int idx = Integer.parseInt(br.readLine());
-		
-		System.out.println(str.substring(idx-1, idx));
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String S = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        char c = S.charAt(N-1);
+
+        bw.write(c+"");
+
+        bw.flush();
+        bw.close();
+
+
+    }
 }
