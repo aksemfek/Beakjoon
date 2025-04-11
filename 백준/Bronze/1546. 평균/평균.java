7551d6e66a5ecaf852e8d86c61bf66e4bf7b81a1
aksemfek <130041317+aksemfek@users.noreply.github.com>
2025-04-11 14:34:47 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 17040 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,25 +1,39 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        int[] arr = new int[number];
-        int max = 0;
-        double sum=0;
-        for (int i=0; i< number; i++){
-            arr[i] = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double[] arr = new double[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = Double.parseDouble(st.nextToken());
         }
-        for(int i=0;i<arr.length;i++){
-            if(max<arr[i]){
-                max=arr[i];
-            }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = (arr[i] / arr[N-1]) * 100.0;
         }
-        for(int i =0; i<arr.length;i++){
-            sum = sum+ ((double)arr[i]/max*100);
+
+        double sum =0;
+
+        for(int i = 0; i < N; i++) {
+            sum += arr[i];
         }
-        System.out.println(sum/number);
+
+        bw.write(sum/N + "\n");
+
+        bw.flush();
+        bw.close();
+
 
     }
 }
