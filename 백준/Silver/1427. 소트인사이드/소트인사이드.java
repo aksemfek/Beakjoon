0e5fcc20da34847062a6c5afa4632f48a905c2d0
aksemfek <130041317+aksemfek@users.noreply.github.com>
2025-04-23 17:12:44 +0900

    [Silver V] Title: 소트인사이드, Time: 100 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,18 +1,26 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        char[] arr = br.readLine().toCharArray();
+        String N = br.readLine();
+        Integer[] arr = new Integer[N.length()];
 
-        Arrays.sort(arr);
-
-        for(int i = arr.length-1; i>=0; i--){
-            System.out.print(arr[i]);
+        for (int i = 0; i < N.length(); i++) {
+            arr[i] = N.charAt(i) - '0';  // input → N으로 수정
         }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(arr[i] + ""); // 숫자를 문자로 출력해야 하니까 + ""
+        }
+
+        bw.flush();
+        bw.close();
     }
 }
