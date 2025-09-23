c6a00c1413666f04d9a74811c7e8f9b495a893e3
aksemfek <130041317+aksemfek@users.noreply.github.com>
2025-09-23 21:29:34 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;
-
 class Solution {
     public int[] solution(int n) {
-        int[] answer;
-        if (n % 2 == 0) {
-            answer = new int[n / 2];
-        } else {
-            answer = new int[(n + 1) / 2];
+        int size = 0;
+        
+         if (n % 2 == 0) {        
+            size = n / 2;
+        } else {                 
+            size = n / 2 + 1;
         }
         
-        for (int i = 0; i < answer.length; i++) {
-                answer[i] = (i * 2) + 1;
-            }
+        int[] answer = new int[size];
+        answer[0] = 1;
+        for(int i = 1; i < size; i++){
+            answer[i] = answer[i-1] + 2;
+        }
         return answer;
     }
 }
