798180b9a187633d063d8e9b45cf2239cb6fe51b
aksemfek <130041317+aksemfek@users.noreply.github.com>
2025-09-30 15:16:10 +0900

    [level 0] Title: 모음 제거, Time: 12.76 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for (int i = 0; i < my_string.length(); i++) {
+            if ("aeiou".indexOf(my_string.charAt(i)) == -1) {
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
