af33b842de087e67eece936d3e2b62fdb8260cab
aksemfek <130041317+aksemfek@users.noreply.github.com>
2025-10-01 20:04:11 +0900

    [level 0] Title: 가까운 수, Time: 0.09 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        int min = 101;
+        
+        for(int i = 0; i < array.length; i++){
+            if( min > Math.abs(array[i] - n)){
+                min = Math.abs(array[i] - n);
+                answer = array[i];
+            } else if( min ==  Math.abs(array[i] - n)){
+                if(answer > array[i]){
+                    answer = array[i];
+                }
+            }
+        }
+        
+                               
+        
+        return answer;
+    }
+}
