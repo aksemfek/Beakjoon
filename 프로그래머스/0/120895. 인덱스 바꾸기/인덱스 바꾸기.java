a782a6a43e0da635a3af6156b324688efedcb960
aksemfek <130041317+aksemfek@users.noreply.github.com>
2025-10-02 16:53:52 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 22.70 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        
+        String str1 = my_string.charAt(num1) + "";
+        String str2 = my_string.charAt(num2) + "";
+        
+        for(int i = 0; i < my_string.length(); i++){
+            if( i == num1){
+                answer += str2;
+            } else if( i == num2){
+                answer += str1;
+            }else{
+                answer += my_string.charAt(i);
+            }
+        }
+        
+        
+        return answer;
+    }
+}
