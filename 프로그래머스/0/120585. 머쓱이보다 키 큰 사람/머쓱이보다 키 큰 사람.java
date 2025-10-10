120632d7e3e65a4d0367afb3670d4ff6a8fa3ed1
aksemfek <130041317+aksemfek@users.noreply.github.com>
2025-10-10 15:23:41 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.45 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] array, int height) {
         int answer = 0;
+        Arrays.sort(array);
         
         for(int i = 0; i < array.length; i++){
             if(array[i] > height){
